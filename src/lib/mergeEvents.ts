df907dffc90b17c91ca8fc2b61a4dab27ff1438f
YassinEldeeb <yassineldeeb94@gmail.com>
2022-05-15 00:05:53 +0200

    fix: add the payload of the event

--- src/lib/mergeEvents.ts
+++ src/lib/mergeEvents.ts
@@ -2,5 +2,5 @@ import { Event, PossibleEventsType } from '../types/EventName'
 import path from 'path'
 
 export const handleEvent = (type: Event, payload: PossibleEventsType) => {
-  require(path.join(__dirname, `../events/${type}.ts`)).default()
+  require(path.join(__dirname, `../events/${type}.ts`)).default(payload)
 }
